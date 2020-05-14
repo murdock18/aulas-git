975d21e13f4e2aecf87ec9b17382e6451cb783fc
Paulo César Jr <paulomurdock@gmail.com>
2020-05-14 18:12:22 -0300

    testes tem JS

---
+++ web-page-text/js/principal.js
@@ -0,0 +1,31 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector(".info-imc");
+
+var pesoEhValido = true;
+var alturaEhValida = true;
+
+if (peso <= 0 || peso >= 300) {
+    console.log("Peso inválido");
+    pesoEhValido = false;
+    tdImc.textContent = "Peso Inválido";
+}
+if (altura <= 0 || altura >= 3.00){
+    console.log ("Altura inválida");
+    alturaEhValida = false
+    tdImc.textContent = "Altura Inválida";
+}
+
+if (alturaEhValida && pesoEhValido){
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
